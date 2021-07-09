88290389fddf2517f000dcc9b2318c8f0f6ee184
Jay Kim <jkim2@grubhub.com>
2021-07-08 22:22:48 -0500

    0.0.b8 better stack trace

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from setuptools.command.install import install
 
 # circleci.py version
-VERSION = "v0.0.b7"
+VERSION = "v0.0.b8"
 
 def readme():
     """print long description"""
