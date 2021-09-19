242d22ca1a98c84415bd60ffed8d05b1039077aa
Jay Kim <jkim2@grubhub.com>
2021-09-19 16:23:23 -0500

    0.0.b37 bug fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from setuptools.command.install import install
 
 # circleci.py version
-VERSION = "v0.0.b36"
+VERSION = "v0.0.b37"
 
 def readme():
     """print long description"""
