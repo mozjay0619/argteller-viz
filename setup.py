756bb892bd874a737fd3b7111e74a49b3e32dc4c
Jay Kim <jkim2@grubhub.com>
2021-11-14 14:13:11 -0600

    uped vers

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from setuptools.command.install import install
 
 # circleci.py version
-VERSION = "v0.0.b37"
+VERSION = "v0.0.b39"
 
 def readme():
     """print long description"""
